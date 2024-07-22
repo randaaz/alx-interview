e2da87ddb7de33e0181f24ca6354bdc03ab191b1
randaaz <randaaziz972@gmail.com>
2024-07-22 01:08:44 -0700

    0-making_change.py

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""This module contains a function to determine the minimum number of coins.
+"""
+
+
+def makeChange(coins, total):
+    """Determines the fewest number of coins
+    needed to make a given total amount.
+    """
+    if total <= 0:
+        return 0
+    rem = total
+    coins_count = 0
+    coin_idx = 0
+    sorted_coins = sorted(coins, reverse=True)
+    n = len(coins)
+    while rem > 0:
+        if coin_idx >= n:
+            return -1
+        if rem - sorted_coins[coin_idx] >= 0:
+            rem -= sorted_coins[coin_idx]
+            coins_count += 1
+        else:
+            coin_idx += 1
+    return coins_count
